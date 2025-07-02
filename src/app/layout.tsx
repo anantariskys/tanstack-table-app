169251b47c8467a288871b76e246a8be270ee98e
anantariskys <gamersindo555@gmail.com>
2025-07-02 15:27:49 +0700

    refactor(layout): simplify layout component by extracting app shell logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,29 @@
-"use client";
 import "@mantine/core/styles.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
 import {
-  AppShell,
-  Burger,
   ColorSchemeScript,
-  MantineProvider,
   mantineHtmlProps,
 } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import RootAppLayout from "@/components/AppShell";
+import { Providers } from "@/components/Providers";
+
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [opened, { toggle }] = useDisclosure();
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="en" {...mantineHtmlProps} className={jakarta.className}>
       <head>
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>
-          <AppShell
-            header={{ height: 60 }}
-            navbar={{
-              width: 300,
-              breakpoint: "sm",
-              collapsed: { mobile: !opened },
-            }}
-            padding="md"
-          >
-            <AppShell.Header>
-              <Burger
-                opened={opened}
-                onClick={toggle}
-                hiddenFrom="sm"
-                size="sm"
-              />
-              <div>Logo</div>
-            </AppShell.Header>
-
-            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-
-            <AppShell.Main> {children}</AppShell.Main>
-          </AppShell>
-        </MantineProvider>
+        <Providers>
+          <RootAppLayout>{children}</RootAppLayout>
+        </Providers>
       </body>
     </html>
   );
