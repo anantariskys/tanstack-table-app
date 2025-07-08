a9703dfd3869b3bc0fd18724c9371ce357a28cd5
anantariskys <gamersindo555@gmail.com>
2025-07-08 14:30:07 +0700

    feat(axios): add response interceptor to format error messages

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,3 +3,21 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.errors &&
+      Array.isArray(error.response.data.errors)
+    ) {
+      const messages = error.response.data.errors.map((err: { detail: string }) => err.detail);
+
+      error.message = messages.join(', ');
+    }
+
+    return Promise.reject(error);
+  },
+);
