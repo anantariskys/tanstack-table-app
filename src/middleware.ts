8ab4fd0d4147f9ba58bba0d3549bfa38c04e5bca
anantariskys <gamersindo555@gmail.com>
2025-07-10 16:22:33 +0700

    fix(middleware): update matcher config to exclude static assets

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from '@/utils/auth';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:js|css|png|jpg|jpeg|svg|ico|woff2?)$).*)',
+  ],
 };
